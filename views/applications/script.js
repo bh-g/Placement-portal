b617cc826ae87d3138de4f8cbf6ac05c8c9eaf8e
Bhumika Gupta <bhumikagupta813@gmail.com>
2024-03-21 15:28:09 +0530

    Modifying sidebar and applications table

--- views/applications/script.js
+++ views/applications/script.js
@@ -11,15 +11,14 @@ function closeNav() {
 
 }
 
-function myFunction() {
-  // Declare variables
+// Function to search for job roles in the table
+function search() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     if (td) {
@@ -32,3 +31,26 @@ function myFunction() {
     }
   }
 }
+
+/* let applications =[{
+    jobPost:'Google SWE Intern 2025', company: 'Google',
+status: 'Shortlisted', details: "#"}] */
+
+/* function filter() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("mylist");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+} */
