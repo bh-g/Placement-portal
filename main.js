a6727d284cb758b89db9242927b3af2a829231ca
manya chandna <manyachandna2122@gmail.com>
2024-03-21 13:36:44 +0530

    redirecting to job

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const loginMessage = document.getElementById('login-message');
   
 	if (username === '036' && password === '036') {
 	  // Login successful, redirect to job postings page (replace with actual redirection)
-	  window.location.href = 'homePage.html';
+	  window.location.href = './views/jobs/jobs.html';
 	} else {
 	  loginMessage.textContent = 'Invalid username or password';
 	}
